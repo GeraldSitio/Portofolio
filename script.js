16131e4afe35632a8cf1d8b3a88b0776702f76e4
geraldsitio <geraldsitio16@gmail.com>
2024-11-02 16:19:35 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// Back to Top
+const backToTopButton = document.getElementById("backToTop");
+
+window.onscroll = function () {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        backToTopButton.style.display = "block";
+    } else {
+        backToTopButton.style.display = "none";
+    }
+};
+
+backToTopButton.onclick = function () {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+};
